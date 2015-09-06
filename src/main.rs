1696678e604d7b9a1de71103bcecbfa15cc6d05f
David Flemström <david.flemstrom@gmail.com>
2015-09-07 00:13:46 +0200

    Switch to rustbox from rustty

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
-extern crate rustty;
+extern crate rustbox;
 
-use rustty::{Event,Terminal};
+use rustbox::{Color,Event,Key,RustBox};
 
 fn main() {
-    let mut term = Terminal::new().unwrap();
+    let rb = RustBox::init(Default::default()).unwrap();
     loop {
-        let evt = term.get_event(-1).unwrap();
-        if let Some(Event::Key(ch)) = evt {
-            match ch {
-                'q' => break,
-                _ => (),
-            }
+        match rb.poll_event(false).unwrap() {
+            Event::KeyEvent(key) => {
+                if key == Some(Key::Char('q')) {
+                    break;
+                }
+
+                rb.clear();
+                if let Some(k) = key {
+                    let visual = fmt_emacs_key(k);
+                    rb.print(0, 0, rustbox::RB_BOLD, Color::White, Color::Black, &visual);
+                }
+            },
+            _ => { }
         }
-        term.swap_buffers().unwrap();
+        rb.present();
+    }
+}
+
+fn fmt_emacs_key(key: Key) -> String {
+    match key {
+        Key::Tab => String::from("tab"),
+        Key::Enter => String::from("return"),
+        Key::Esc => String::from("escape"),
+        Key::Backspace => String::from("backspace"),
+        Key::Right => String::from("right"),
+        Key::Left => String::from("left"),
+        Key::Up => String::from("up"),
+        Key::Down => String::from("down"),
+        Key::Delete => String::from("delete"),
+        Key::Home => String::from("home"),
+        Key::End => String::from("end"),
+        Key::PageUp => String::from("prior"),
+        Key::PageDown => String::from("next"),
+        Key::Char(c) => c.to_string(),
+        Key::Ctrl(c) => format!("C-{}", c),
+        Key::F(n) => format!("f{}", n),
     }
 }
