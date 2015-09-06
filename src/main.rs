9490a57615076e7cab9727b880fefe9b41b63d51
David Flemström <david.flemstrom@gmail.com>
2015-09-06 23:05:56 +0200

    Properly isolate platform deploys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate rustty;
+
+use rustty::{Event,Terminal};
+
 fn main() {
-    println!("Hello, world!");
+    let mut term = Terminal::new().unwrap();
+    loop {
+        let evt = term.get_event(-1).unwrap();
+        if let Some(Event::Key(ch)) = evt {
+            match ch {
+                'q' => break,
+                _ => (),
+            }
+        }
+        term.swap_buffers().unwrap();
+    }
 }
